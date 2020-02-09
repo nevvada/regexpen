88be247b7b68737b8b7601fc9ad0cabb7697c7bc
nlemast <nlemaster47@gmail.com>
2020-02-08 19:10:15 -0500

    Create state, methods for updating regex and text, pass down props

--- src/RegexTextContainer.tsx
+++ src/RegexTextContainer.tsx
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
 
-import TextBox from './TextBox';
 import RegexBox from './RegexBox';
+import TextBox from './TextBox';
+
+interface State {
+  regexText: string;
+  userText: string;
+}
 
 export class RegexTextContainer extends Component {
+  state: State = {
+    regexText: '',
+    userText: '',
+  }
+
+  updateRegex = () => {
+
+  }
+
+  updateText = (event: any) => {
+    const { target: { value } } = event;
+
+    this.setState({ userText: value });
+  }
+
   render() {
+    const { regexText, userText } = this.state;
+
     return (
       <>
-        <TextBox />
-        <RegexBox />
+        <RegexBox 
+          regexText={regexText} 
+        />
+        <TextBox 
+          updateText={this.updateText}
+          userText={userText}
+        />
       </>
     )
   }
