6de31b996b8f1f4bfd5e4110a39912d2428bfa39
nlemast <nlemaster47@gmail.com>
2020-02-10 20:28:18 -0500

    Add dev dependencies and configure babel to resolve breaking build

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
-  plugins: ['@babel/plugin-transform-react-jsx', '@babel/plugin-proposal-class-properties']
+  presets: ['@babel/preset-react', '@babel/preset-env', '@babel/preset-typescript'],
+  plugins: ['@babel/plugin-transform-runtime', '@babel/plugin-transform-react-jsx', '@babel/plugin-proposal-class-properties']
 };
