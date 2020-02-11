d7e89dff13f86ae92cc0d6bf5dff66a791faa28f
nlemast <nlemaster47@gmail.com>
2020-02-10 20:27:35 -0500

    Resolve TS error with ReactDOM.render

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import { render } from 'react-dom';
+
 import App from './App';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+render(React.createElement(App), document.querySelector('#root'));
