b965b2f99cc8c0f4626b00bee04e5a29674daa89
nlemast <nlemaster47@gmail.com>
2020-02-10 21:18:17 -0500

    Built out skeleton of App container with simple styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 
 import RegexTextContainer from './RegexTextContainer';
 
+import '../scss/main.scss';
 export class App extends Component {
   render() {
     return (
       <>
-        <p>Hello, welcome to my template.</p>
-        <RegexTextContainer />
+        <main id="app-wrapper">
+          <header>
+            <h1>Hello, welcome to my RegexPen.</h1>
+          </header>
+
+          <RegexTextContainer />
+        </main>
       </>
     );
   }
