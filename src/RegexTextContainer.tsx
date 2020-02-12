41f2e0272506f295c1a11fb3760ea94423461e4a
nlemast <nlemaster47@gmail.com>
2020-02-11 21:08:23 -0500

    Refactor updateRegexText method to update state of regexText state value; change name of userText -> inputText

--- src/RegexTextContainer.tsx
+++ src/RegexTextContainer.tsx
@@ -4,37 +4,39 @@ import RegexBox from './RegexBox';
 import TextBox from './TextBox';
 
 interface State {
+  inputText: string;
   regexText: string;
   userText: string;
 }
 
 export class RegexTextContainer extends Component {
   state: State = {
+    inputText: '',
     regexText: '',
     userText: '',
   }
 
-  updateRegex = () => {
+  updateRegexText = (event: any) => {
+    const { inputText } = this.state;
+    const { target: { value } } = event;
 
+    this.setState({ regexText: value });
   }
 
   updateText = (event: any) => {
     const { target: { value } } = event;
 
-    this.setState({ userText: value });
+    this.setState({ inputText: value });
   }
 
   render() {
-    const { userText } = this.state;
-
     return (
       <>
         <RegexBox 
-          userText={userText} 
+          updateRegexText={this.updateRegexText}
         />
         <TextBox 
           updateText={this.updateText}
-          userText={userText}
         />
       </>
     )
