671712249ae85c96bc29aa0476f186c00bbd254c
nlemast <nlemaster47@gmail.com>
2020-02-17 16:19:33 -0500

    Attempt useEffect to add spans for each character displayed from text input

--- src/TextBox.tsx
+++ src/TextBox.tsx
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 interface Props {
   updateText: (arg: any) => void;
+  updatedText: string;
 }
 
-const TextBox = ({ updateText }: Props) => {
+const addSpans = (text: string) => {
+  return [...text].map((each, index) => {
+    return (
+    <span 
+      key={each + index}
+      style={{color: 'red'}}>
+        {each}
+    </span>)
+  })
+}
 
+const TextBox = ({ highlightInputText, regexText, updateText, updatedText }: Props) => {
+  useEffect(() => {
+    highlightInputText(updatedText)
+  }, [regexText])
+  
   return (
     <section className="text-box">
       <form>
-        <input type="text" onChange={updateText} />  
+        <input type="text" onChange={updateText} />
       </form> 
+      <p>{addSpans(updatedText)}</p>
     </section>
   )
 }
